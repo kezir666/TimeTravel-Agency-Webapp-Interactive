cb574747f64229962dbb591930ba88dadaed13df
Mateo NICOUD <mateo.nicoud@gmail.com>
2026-02-17 15:18:31 +0100

    fix: images

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/TimeTravel-Agency-Webapp-Interactive',
+  assetPrefix: '/TimeTravel-Agency-Webapp-Interactive/',
   typescript: {
     ignoreBuildErrors: true,
   },
